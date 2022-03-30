284927ff9252500170cd44abc8e9b0eaf6bbd278
Julio Lins <juliolinns10@gmail.com>
2022-03-30 15:40:15 -0300

    newCommit2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 using Backend.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
